36ec68c6bc186bf817e65e15dda9b1980effd233
ayaka <song.0027@iris.eonet.ne.jp>
2026-03-13 04:51:47 +0900

    fix:デプロイ修正

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root to: redirect("/up")
+
   namespace :api do
     resources :mental_check_results, only: [ :index, :create, :show ]
   end
